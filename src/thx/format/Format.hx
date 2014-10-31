e3b95bb42ad31d4948a2083695282df693302597
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-31 13:03:07 -0600

    cleanedup Format.f

--- src/thx/format/Format.hx
+++ src/thx/format/Format.hx
@@ -1,16 +1,12 @@
 package thx.format;
 
+import haxe.macro.Context;
 import haxe.macro.Expr;
+import haxe.macro.TypeTools;
 
 class Format {
-  macro public static function f(subject : Expr, pattern : ExprOf<String>, ?culture : ExprOf<thx.culture.Culture>) {
-    trace(haxe.macro.ExprTools.toString(subject));
-
-    var t = haxe.macro.Context.typeof(subject);
-    trace(t);
-
-
-    return switch haxe.macro.TypeTools.toString(t) {
+  macro public static function f(subject : Expr, pattern : ExprOf<String>, ?culture : ExprOf<thx.culture.Culture>)
+    return switch TypeTools.toString(Context.typeof(subject)) {
       case "Date":
         macro thx.format.DateFormat.format($e{subject}, $e{pattern}, $e{culture});
       case "Int", "Float":
@@ -18,11 +14,4 @@ class Format {
       case t:
         haxe.macro.Context.error('Unsupported type "$t" for formatting', subject.pos);
     };
-
-    haxe.macro.ExprTools.map(subject, function(e) {
-      trace(e);
-      return e;
-    });
-    return pattern;
-  }
 }
