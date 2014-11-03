6326de3d97a02df999ca8de0302b68453775386b
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-02 18:05:36 -0700

    some doc

--- src/thx/format/Format.hx
+++ src/thx/format/Format.hx
@@ -5,6 +5,14 @@ import haxe.macro.Expr;
 import haxe.macro.TypeTools;
 
 class Format {
+/**
+Applies the right format according to the type of subject.
+
+```haxe
+Date.now().f("D"); // returns the date in long format
+12345.f("n", it);  // returns a number formatted using the provided culture
+```
+*/
   macro public static function f(subject : Expr, pattern : ExprOf<String>, ?culture : ExprOf<thx.culture.Culture>)
     return switch TypeTools.toString(Context.typeof(subject)) {
       case "Date":
