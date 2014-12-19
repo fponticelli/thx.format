f27cb82552a1859e00f79c2f17d07d757d07de5d
Jason O'Neil <jason.oneil@gmail.com>
2014-12-19 11:59:22 +0800

    Get Format.f() to follow typedefs.
    So that it works with `sys.db.Types.SDate` etc
    
    Fixes #9

--- test/thx/format/TestFormat.hx
+++ test/thx/format/TestFormat.hx
@@ -3,6 +3,7 @@ package thx.format;
 import thx.culture.Culture;
 import thx.culture.Embed;
 import utest.Assert;
+import sys.db.Types;
 using thx.format.Format;
 
 class TestFormat {
@@ -11,7 +12,10 @@ class TestFormat {
   public function new() {}
 
   public function testF() {
-    Assert.equals("Wednesday, 01 January 2014", Date.fromString("2014-01-01").f("D"));
+    var date = Date.fromString("2014-01-01");
+    var sDate:SDate = date;
+    Assert.equals("Wednesday, 01 January 2014", date.f("D"));
+    Assert.equals("Wednesday, 01 January 2014", sDate.f("D"));
     Assert.equals("1,000.00", 1000.f("n"));
 
     Assert.equals("mercoledì 1 gennaio 2014", Date.fromString("2014-01-01").f("D", it));
