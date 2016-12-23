a3d4fc9845adfd14306a44dd19f95c27b1be4a72
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-12-23 09:53:09 -0700

    update docs

--- doc/ImportFormat.hx
+++ doc/ImportFormat.hx
@@ -1,3 +1,7 @@
+import thx.format.BigIntFormat;
 import thx.format.DateFormat;
+import thx.format.DecimalFormat;
 import thx.format.Format;
+import thx.format.LocalDateFormat;
 import thx.format.NumberFormat;
+import thx.format.TimeFormat;
